9a517b57a5bf9afc3ae3da176f581c967d087cbb
Matheus Felisberto <matheus.felisberto@hotmail.com>
2020-06-18 00:20:58 -0300

    add(sort): heap sort imperative implementation

--- HeapSort.scala
+++ HeapSort.scala
@@ -40,3 +40,44 @@ object HeapSort {
   def sort(xs: Seq[Int]): Seq[Int] =
     toList(xs.foldLeft(EmptyHeap: Heap[Int])((memo, x) => merge(HeapSort(x), memo)))
 }
+
+object HeapSortImperative {
+  def heapify(a: Array[Int], n: Int, i: Int): Unit = {
+    var largest = i
+    var l = 2 * i + 1
+    var r = 2 * i + 2
+    
+    if (l < n && a(i) < a(l)) {
+      largest = l
+    }
+    
+    if (r < n && a(largest) < a(r)) {
+      largest = r
+    }
+    
+    if (largest != i) {
+      val tmp = a(i)
+      a(i) = a(largest)
+      a(largest) = tmp
+      
+      heapify(a, n, largest)
+    }
+  }
+  
+  def sort(a: Array[Int]) = {
+    val n = a.length
+
+    for (i <- n / 2 - 1 to 0 by -1 ) {
+      println(i)
+      heapify(a, n, i)
+    }
+    
+    for (i <- n - 1 to 0 by -1) {
+      val tmp = a(i)
+      a(i) = a(0)
+      a(0) = tmp
+      
+      heapify(a, i, 0)
+    }
+  } 
+}
